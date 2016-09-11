a3eefc220df2f1780d5336f240f161eb44a47046
Yuya Aratani <hrgruri@gmail.com>
2016-09-11 16:40:56 +0900

    add Draft, Init Test

---
+++ tests/CommandTest/DraftTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use Symfony\Component\Console\Tester\CommandTester;
+use org\bovigo\vfs\{
+    vfsStream,
+    vfsStreamWrapper,
+    vfsStreamDirectory
+};
+
+class DraftTest extends TestCase
+{
+    private $root;
+
+    public function setUp()
+    {
+        parent::setUp();
+        vfsStreamWrapper::register();
+        vfsStreamWrapper::setRoot(new vfsStreamDirectory('saori'));
+        $this->root = vfsStream::url('saori');
+    }
+
+    public function testExecute()
+    {
+        $app = new Hrgruri\Saori\Application($this->root);
+        $command = $app->find('draft');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => $command->getName(), 'name' => 'test']);
+        $data = json_decode(file_get_contents("{$this->root}/draft/test/config.json"));
+        $this->assertTrue(vfsStreamWrapper::getRoot()->hasChild('draft'));
+        $this->assertTrue(vfsStreamWrapper::getRoot()->hasChild('draft/test/config.json'));
+        $this->assertTrue(vfsStreamWrapper::getRoot()->hasChild('draft/test/article.md'));
+        $this->assertEquals('test', $data->title);
+        $this->assertInternalType('array', $data->tag);
+        $this->assertEquals(0, count($data->tag));
+    }
+
+    public function testExecuteException()
+    {
+        $app = new Hrgruri\Saori\Application($this->root);
+        $command = $app->find('draft');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => $command->getName(), 'name' => 'test']);
+        $this->assertNotRegExp('/already\ exists$/i', $commandTester->getDisplay());
+        $commandTester->execute(['command' => $command->getName(), 'name' => 'test']);
+        $this->assertRegExp('/already\ exists$/i', $commandTester->getDisplay());
+
+    }
+}
---
+++ tests/CommandTest/InitTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Symfony\Component\Console\Tester\CommandTester;
+use org\bovigo\vfs\{
+    vfsStream,
+    vfsStreamWrapper,
+    vfsStreamDirectory
+};
+
+class InitTest extends TestCase
+{
+    private $root;
+
+    public function setUp()
+    {
+        parent::setUp();
+        vfsStreamWrapper::register();
+        vfsStreamWrapper::setRoot(new vfsStreamDirectory('saori'));
+        $this->root = vfsStream::url('saori');
+    }
+
+    public function testExecute()
+    {
+        $app = new Hrgruri\Saori\Application($this->root);
+        $command = $app->find('init');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => $command->getName()]);
+        $data = json_decode(file_get_contents("{$this->root}/contents/config.json"));
+
+        $this->assertRegExp('/^done$/i', $commandTester->getDisplay());
+        $this->assertTrue(vfsStreamWrapper::getRoot()->hasChild('contents'));
+        $this->assertTrue(vfsStreamWrapper::getRoot()->hasChild('contents/config.json'));
+        $this->assertEquals('username', $data->id);
+        $this->assertInstanceOf(\stdClass::class, $data->link);
+    }
+
+    public function testExecuteException()
+    {
+        $app = new Hrgruri\Saori\Application($this->root);
+        $command = $app->find('init');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => $command->getName()]);
+        $this->assertRegExp('/^done$/i', $commandTester->getDisplay());
+        $commandTester->execute(['command' => $command->getName()]);
+        $this->assertNotRegExp('/^done$/i', $commandTester->getDisplay());
+    }
+}
