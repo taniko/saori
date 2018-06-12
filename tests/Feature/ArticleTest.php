cf194b8ff54379416cecd510a8eff628379c69c6
Yuya Aratani <hrgruri@gmail.com>
2018-06-12 17:29:16 +0900

    fix create articles test

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -1,11 +1,13 @@
 <?php
 namespace Test\Feature;
 
+use Taniko\Saori\Generator\ArticleGenerator;
+
 class ArticleTest extends \TestCase
 {
     public function testCreate()
     {
-        $this->generateArticleFile("{$this->root}/contents");
-        $this->assertTrue(true);
+        $this->assertTrue($this->generateArticleFile("{$this->root}/contents"));
+        $this->assertEquals(1, ArticleGenerator::getArticles("{$this->root}/contents")->count());
     }
 }
